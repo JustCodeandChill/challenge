98119015760f54a9ae9852f8956b5869e65a2d44
JustCodeandChill <inforreceiver@gmail.com>
2021-08-13 00:27:08 -0400

    Set up webpack in client side

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Home from './components/Home.jsx';
+
+export default function App() {
+    return (
+        <div>
+            Hi I am App
+            <Home></Home>
+        </div>
+    )
+}
---
+++ client/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: { index: path.resolve(__dirname, "src", "index.js") },
+  output: {
+    path: path.resolve(__dirname, "../",  "public"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      { test: /\.(png|jp(e*)g|svg|gif)$/, use: ["file-loader"] },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"),
+    //  favicon: path.resolve(__dirname, "../",  "public", "favicon.png"),
+    }),
+  ],
+  optimization: {
+    splitChunks: { chunks: "all" },
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
+};
